3da33a36f83289473f7b10e7641865caafd0517b
Dominic H <dom@dominic.sk>
2021-11-19 02:01:58 +0000

    Upload scraper
    Added; Cases and Deaths data

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-CovidDashboardScraper
+# Covid Dashboard Scraper
+
+Python XPath scraper for https://coronavirus.data.gov.uk/
+
+***
+
+## Functions
+
+**COVID-19 Case Data**
+
+`getDailyCases()` - Returns positive COVID-19 cases over the last 24 hour period
+
+`getCases7DaysIncDec()` - Returns the increase/decrease in COVID-19 cases over the last 7 days
+
+`getCases7DaysIncDecPercentage()` - Returns the previous 7 day percentage increase/decrease in COVID-19 cases
+
+**COVID-19 Death Data**
+
+`getDailyDeaths()` - Returns confirmed deaths over 24 hours that were within 28 days of a positive test result
+
+`getDeaths7DaysIncDec()` - Returns the increase/decrease in COVID-19 death over the last 7 days
+
+`getDeaths7DaysIncDecPercentage()` - Returns the previous 7 day percentage increase/decrease in COVID-19 deaths
---
+++ main.py
@@ -0,0 +1,33 @@
+from lxml import html
+import requests
+
+scraperHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
+scraperEndpoint = requests.get('https://coronavirus.data.gov.uk/', headers=scraperHeaders)
+tree = html.fromstring(scraperEndpoint.content)
+
+def getDailyCases():
+    casesDaily = tree.xpath('/html/body/div[4]/div[2]/div/main/article[1]/ul/li[2]/div[1]/ul/li[1]/div[2]/div/div/div/span[1]/text()')
+    return casesDaily[0]
+
+def getCases7DaysIncDec():
+    prev7DaysCasesIncDec = tree.xpath('//*[@id="main-content"]/article[1]/ul/li[2]/div[1]/ul/li[3]/div/div/span/b/span/strong/text()')
+    return prev7DaysCasesIncDec[0]
+
+def getCases7DaysIncDecPercentage():
+    prev7DaysCasesIncDecPercent = tree.xpath('/html/body/div[4]/div[2]/div/main/article[1]/ul/li[2]/div[1]/ul/li[3]/div/div/span/b/span/text()')
+    return prev7DaysCasesIncDecPercent[0]
+
+def getDailyDeaths():
+    deathsDaily = tree.xpath('/html/body/div[4]/div[2]/div/main/article[1]/ul/li[3]/div[1]/ul/li[1]/div[2]/div/div/div/span[1]/text()')
+    return deathsDaily[0]
+
+def getDeaths7DaysIncDec():
+    prev7DaysDeathsIncDec = tree.xpath('/html/body/div[4]/div[2]/div/main/article[1]/ul/li[3]/div[1]/ul/li[3]/div/div/span/b/span/strong/text()')
+    return prev7DaysDeathsIncDec[0]
+
+def getDeaths7DaysIncDecPercentage():
+    prev7DaysDeathsIncDecPercent = tree.xpath('/html/body/div[4]/div[2]/div/main/article[1]/ul/li[3]/div[1]/ul/li[3]/div/div/span/b/span/text()')
+    return prev7DaysDeathsIncDecPercent[0]
+
+#Example
+#print(getDeaths7DaysIncDec())
